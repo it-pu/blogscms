687d09007e61fccf7355f44193c75a959a287180
Ahmad Kashabul Yamin <ahmad.yamin@podomorouniversity.ac.id>
2020-02-06 09:09:17 +0700

    dashboard

--- application/views/template/V_content.php
+++ application/views/template/V_content.php
@@ -30,33 +30,35 @@
 		<!-- /Breadcrumbs line -->
 
 		<!--=== Page Header ===-->
-		<div class="page-header ">
-			<div class="page-title ">
-				<h1>Dashboard</h1>
-				<span><h4>Coming Soon!!!</h4></span>
-			</div>
+		<div class="col-md-4 col-md-offset-4 text-center ">
+			<div class="page-header ">
+				<div class="">
+					<h1>DASHBOARD</h1>
+					<span><h1>COMING SOON!!!</h1></span>
+				</div>
 
-			<!-- Page Stats -->
-			<!-- <ul class="page-stats">
-				<li>
-					<div class="summary">
-						<span>New orders</span>
-						<h3>17,561</h3>
-					</div>
-					<div id="sparkline-bar" class="graph sparkline hidden-xs">20,15,8,50,20,40,20,30,20,15,30,20,25,20</div>
-					 Use instead of sparkline e.g. this:
-					<div class="graph circular-chart" data-percent="73">73%</div>
-					
-				</li>
-				<li>
-					<div class="summary">
-						<span>My balance</span>
-						<h3>$21,561.21</h3>
-					</div>
-					<div id="sparkline-bar2" class="graph sparkline hidden-xs">20,15,8,50,20,40,20,30,20,15,30,20,25,20</div>
-				</li>
-			</ul> -->
-			<!-- /Page Stats -->
+				<!-- Page Stats -->
+				<!-- <ul class="page-stats">
+					<li>
+						<div class="summary">
+							<span>New orders</span>
+							<h3>17,561</h3>
+						</div>
+						<div id="sparkline-bar" class="graph sparkline hidden-xs">20,15,8,50,20,40,20,30,20,15,30,20,25,20</div>
+						 Use instead of sparkline e.g. this:
+						<div class="graph circular-chart" data-percent="73">73%</div>
+						
+					</li>
+					<li>
+						<div class="summary">
+							<span>My balance</span>
+							<h3>$21,561.21</h3>
+						</div>
+						<div id="sparkline-bar2" class="graph sparkline hidden-xs">20,15,8,50,20,40,20,30,20,15,30,20,25,20</div>
+					</li>
+				</ul> -->
+				<!-- /Page Stats -->
+			</div>
 		</div>
 		<!-- /Page Header -->
 
